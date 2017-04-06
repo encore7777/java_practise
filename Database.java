cfcdd2861e6060b2d9388e10dc4ec34c490e55e8
encor3 <chachanyk@163.com>
2017-04-06 20:21:46 +0800

    database code for java
    a dome class to restore the CD/DVD/Games.

---
+++ Database.java
@@ -0,0 +1,50 @@
+package dome;
+
+import java.util.ArrayList;
+
+public class Database {
+	
+//	public ArrayList<CD> listCD = new ArrayList <CD>();
+//	public ArrayList<DVD> listDVD = new ArrayList <DVD>();
+	public ArrayList<ITEM> listITEM = new ArrayList <ITEM>();
+	
+	public void add(ITEM item){
+		listITEM.add(item);		
+	}
+	
+	
+	public void list(){
+      for (ITEM item: listITEM){
+    	item.print();  
+      }  
+      
+  	}
+      
+	
+
+
+	
+
+
+	public static void main(String[] args){
+		Object o = new Object();
+//		o.toString();
+		
+		Database db = new Database();
+		ITEM cd = new CD("abc","def",4,60,"good");
+		ITEM cc = new CD("abc","def",4,60,"good");
+        
+		System.out.println(cd.equals(cc));
+//		System.out.println(cd);
+//		db.add(cd);
+//		
+//		ITEM dvd = new DVD("ABC","DEF",100,"bad");
+//		db.add(dvd);
+//		ITEM game = new Game("i","ii",70,"iii");
+//		db.add(game);
+//		
+//		db.list();
+	}
+	
+
+}
